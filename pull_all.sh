28332e5edd781c812f5b1274f1e15cdc21f9b9f4
gwq5210 <gwq5210@qq.com>
2022-10-18 10:57:22 +0800

    update scrapyd

--- pull_all.sh
+++ pull_all.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 cd `dirname "$0"`
 
-image_name_list=$(grep image *.yml | awk '{print $3}' | sort | uniq)
+image_name_list=$(grep image *.yml | sed 's/#.*//g' | awk '{print $3}' | sort | uniq)
 
 for image_name in $image_name_list
 do
+  if [ -z "$image_name" ] ; then
+    continue
+  fi
+  echo docker pull $image_name
   docker pull $image_name
 done
