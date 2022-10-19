30bf58784b1b81154f8bb8581e4b5213c501df71
gwq5210 <gwq5210@qq.com>
2022-10-19 18:04:45 +0800

    update portainer

--- setup.sh
+++ setup.sh
@@ -10,10 +10,47 @@ docker stack rm setup_certs
 ./setup_secrets.sh
 
 DOCKER_SITE_KEY_DATA_DIR=$(cat .env | grep "DOCKER_SITE_KEY_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
+DOCKER_SITE_DATA_DIR=$(cat .env | grep "DOCKER_SITE_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
+DOCKER_SITE_CONF_DIR=$(cat .env | grep "DOCKER_SITE_CONF_DIR" | egrep -v "^#" | awk -F = '{print $2}')
+DOCKER_SITE_LOGS_DIR=$(cat .env | grep "DOCKER_SITE_LOGS_DIR" | egrep -v "^#" | awk -F = '{print $2}')
+FILE_BROWSER_SHARE_DATA_DIR=$(cat .env | grep "FILE_BROWSER_SHARE_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
+TANK_SHARE_DATA_DIR=$(cat .env | grep "TANK_SHARE_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
+DOCKER_SITE_BACKUP_DIR=$(cat .env | grep "DOCKER_SITE_BACKUP_DIR" | egrep -v "^#" | awk -F = '{print $2}')
 es_config_dir=${DOCKER_SITE_KEY_DATA_DIR}/elasticsearch/config
 kibana_config_dir=${DOCKER_SITE_KEY_DATA_DIR}/kibana/config
 mkdir -p $es_config_dir $kibana_config_dir
 chmod 777 $es_config_dir $kibana_config_dir
+
+dir_array=()
+dir_array[${#dir_array[@]}]=$FILE_BROWSER_SHARE_DATA_DIR
+dir_array[${#dir_array[@]}]=$TANK_SHARE_DATA_DIR
+dir_array[${#dir_array[@]}]=${TANK_SHARE_DATA_DIR}/gwq5210/root
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/nginx
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/mysql
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_KEY_DATA_DIR}/mysql
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_CONF_DIR}/tank
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/tank
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/v2ray
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_CONF_DIR}/filebrowser
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/scrapyd
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/scrapyd
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/mirai/bots
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/mirai/plugins
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/mirai
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_CONF_DIR}/mirai
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_KEY_DATA_DIR}/elasticsearch/data
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_KEY_DATA_DIR}/aliyunpan/sync_drive
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_BACKUP_DIR}/elasticsearch
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_BACKUP_DIR}/mysql
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/jiacrontab
+dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/scrapydweb
+
+for dir in ${dir_array[@]};
+do
+  mkdir -p $dir
+done
+
+chmod 777 ${DOCKER_SITE_KEY_DATA_DIR}/elasticsearch/data
 
 docker stack rm setup_keystore
 # docker-compose -f setup-docker-compose.yml config | docker stack deploy -c - setup
--- start.sh
+++ start.sh
@@ -4,44 +4,5 @@ cd `dirname "$0"`
 
 # docker-compose --compatibility up -d $@
 
-DOCKER_SITE_KEY_DATA_DIR=$(cat .env | grep "DOCKER_SITE_KEY_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
-DOCKER_SITE_DATA_DIR=$(cat .env | grep "DOCKER_SITE_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
-DOCKER_SITE_CONF_DIR=$(cat .env | grep "DOCKER_SITE_CONF_DIR" | egrep -v "^#" | awk -F = '{print $2}')
-DOCKER_SITE_LOGS_DIR=$(cat .env | grep "DOCKER_SITE_LOGS_DIR" | egrep -v "^#" | awk -F = '{print $2}')
-FILE_BROWSER_SHARE_DATA_DIR=$(cat .env | grep "FILE_BROWSER_SHARE_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
-TANK_SHARE_DATA_DIR=$(cat .env | grep "TANK_SHARE_DATA_DIR" | egrep -v "^#" | awk -F = '{print $2}')
-DOCKER_SITE_BACKUP_DIR=$(cat .env | grep "DOCKER_SITE_BACKUP_DIR" | egrep -v "^#" | awk -F = '{print $2}')
-
-dir_array=()
-dir_array[${#dir_array[@]}]=$FILE_BROWSER_SHARE_DATA_DIR
-dir_array[${#dir_array[@]}]=$TANK_SHARE_DATA_DIR
-dir_array[${#dir_array[@]}]=${TANK_SHARE_DATA_DIR}/gwq5210/root
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/nginx
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/mysql
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_KEY_DATA_DIR}/mysql
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_CONF_DIR}/tank
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/tank
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/v2ray
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_CONF_DIR}/filebrowser
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/scrapyd
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/scrapyd
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/mirai/bots
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/mirai/plugins
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_LOGS_DIR}/mirai
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_CONF_DIR}/mirai
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_KEY_DATA_DIR}/elasticsearch/data
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_KEY_DATA_DIR}/aliyunpan/sync_drive
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_BACKUP_DIR}/elasticsearch
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_BACKUP_DIR}/mysql
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/jiacrontab
-dir_array[${#dir_array[@]}]=${DOCKER_SITE_DATA_DIR}/scrapydweb
-
-for dir in ${dir_array[@]};
-do
-  mkdir -p $dir
-done
-
-chmod 777 ${DOCKER_SITE_KEY_DATA_DIR}/elasticsearch/data
-
 # docker-compose config | docker stack deploy -c - docker_site
 ./stack_deploy.sh docker_site docker-compose.yml
