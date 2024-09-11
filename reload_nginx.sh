04f98c4ddcb81d7ce51afbc4a489a3c16be0594e
gwq5210 <gwq5210@qq.com>
2024-09-11 22:05:58 +0800

    update

--- reload_nginx.sh
+++ reload_nginx.sh
@@ -2,6 +2,6 @@
 
 cd `dirname "$0"`
 
-nginx_id=$(docker ps | grep docker_site_gwq5210_nginx | awk '{print $1}')
+nginx_id=$(docker ps | grep gwq5210_nginx | awk '{print $1}')
 echo docker exec -it $nginx_id nginx -s reload
 docker exec -it $nginx_id nginx -s reload
